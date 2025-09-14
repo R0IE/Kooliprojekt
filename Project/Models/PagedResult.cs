197fabb9d60d0192384e72de14cb6dc6dc1bdeba
R0IE <111665617+R0IE@users.noreply.github.com>
2025-09-15 01:25:03 +0300

    pagination model and view component

---
+++ Project/Models/PagedResult.cs
@@ -0,0 +1,11 @@
+namespace KooliProjekt;
+
+public class PagedResult<T> : PagedResultBase
+{
+    public IList<T> Results { get; set; }
+
+    public PagedResult()
+    {
+        Results = new List<T>();
+    }
+}
