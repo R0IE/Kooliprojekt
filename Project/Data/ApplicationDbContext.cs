4bd2248df18cf3d7e4306d4b819655a041d6fe19
R0IE <111665617+R0IE@users.noreply.github.com>
2025-09-16 01:54:05 +0300

    ApplicationDbContext and add validation classes for entities

--- Project/Data/ApplicationDbContext.cs
+++ Project/Data/ApplicationDbContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Identity.Client;
 
 namespace KooliProjekt.Data
 {
@@ -9,14 +8,15 @@ namespace KooliProjekt.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         { }
-        public DbSet<Project> Projects { get; set; }
+        public DbSet<Project> Project { get; set; }
 
         public DbSet<Tasks> Tasks { get; set; }
 
-        public DbSet<team_members> TeamMembers { get; set; }
+        public DbSet<TeamMembers> Team_Members { get; set; }
 
-    public DbSet<User> AppUsers { get; set; }
+        public DbSet<User> User { get; set; }
 
         public DbSet<WorkLogs> WorkLogs { get; set; }
+
     }
 }
--- Project/Program.cs
+++ Project/Program.cs
@@ -1,43 +1,91 @@
+using FluentValidation;
 using KooliProjekt.Data;
+using KooliProjekt.Data.Validation;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace KooliProjekt
 {
-    app.UseMigrationsEndPoint();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddValidatorsFromAssemblyContaining<ProjectValidator>();
+            builder.Services.AddValidatorsFromAssemblyContaining<TasksValidator>();
+            builder.Services.AddValidatorsFromAssemblyContaining<TeamMembersValidator>();
+            builder.Services.AddValidatorsFromAssemblyContaining<UserValidator>();
+            builder.Services.AddValidatorsFromAssemblyContaining<WorkLogsValidator>();
+            builder.Services.AddScoped<IFileClient, LocalFileClient>();
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+
+#if (DEBUG)
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                dbContext.Database.Migrate();
+
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                IdentityUser user = new();
+                user.UserName = "userd@mail.ee";
+                user.EmailConfirmed = true;
+                user.PasswordHash = userManager.PasswordHasher.HashPassword(user, "heromine");
+
+                userManager.CreateAsync(user).Wait();
+
+                if (dbContext.Project.Count() == 0)
+                {
+                    Project project = new Project();
+
+                    project.Id = "1";
+                    project.ProjectName = "Projekt #1";
+                    project.Start = "2025-10-23";
+                    project.Deadline = "2025-01-23";
+                    project.Budget = "100000";
+                    project.HourlyRate = "100";
+                    project.Team = "Team #1";
+
+                    dbContext.Project.Add(project);
+                    dbContext.SaveChanges();
+                }
+            }
+#endif
+
+            app.Run();
+        }
+    }
 }
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
-
-app.Run();
